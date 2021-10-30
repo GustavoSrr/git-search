2e638fa20e576dc5d8b59c09af722c292e37bea2
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-30 17:30:05 -0300

    ✨ Usando context para buscar por um user

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { FormEvent, useContext, useState } from 'react'
 
-import { Container } from './styles'
+import { FaGithub } from 'react-icons/fa'
+import { VscInfo } from 'react-icons/vsc'
+import { SearchContext } from '../../contexts/search'
+
+import { Container, Left, Right } from './styles'
 
 const Header: React.FC = () => {
+  const [inputValue, setInputValue] = useState('')
+
+  const { searchUser } = useContext(SearchContext)
+
+  function handleSearchForUser(event: FormEvent) {
+    event.preventDefault()
+
+    if(inputValue) {
+      searchUser(inputValue)
+    } else {
+      return
+    }
+  }
   return (
     <Container>
-      <h1>Header</h1>
+      <Left>
+        <FaGithub size="32" />
+        <form onSubmit={(event) => handleSearchForUser(event)}>
+          <input
+            placeholder="Pesquisar"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+        </form>
+      </Left>
+      <Right>
+        <VscInfo size="32" />
+      </Right>
     </Container>
   )
 }
