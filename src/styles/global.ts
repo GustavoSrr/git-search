1ddba090bd97162283feda60b44413fafbae38d0
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-30 17:33:07 -0300

    💄 Criando variáveis para cores

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,23 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --bgColor: #0d1117;
+    --borderColor: #30363d;
+    --headerBgColor: #161b22;
+
+    --blue: #1f6feb;
+    --white: #f0f6fc;
+    --gray: #c9d1d9;
+  }
+
   body {
+    min-height: 100vh;
+
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    background-color: var(--bgColor);
   }
 `
