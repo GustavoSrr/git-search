70a661bf8b50823ccd0d9e0f6c317304c917f35a
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-30 20:58:03 -0300

    ✨ Criando componente UserInfo

---
+++ src/components/UserInfo/index.tsx
@@ -0,0 +1,86 @@
+import React, { useContext } from 'react'
+
+import { SearchContext } from '../../contexts/search'
+
+import {
+  FaGithubAlt,
+  FaLink,
+  FaTwitter,
+  FaMapMarkerAlt,
+  FaRegBuilding,
+  FaRegEnvelope
+} from 'react-icons/fa'
+
+import {
+  Container,
+  Avatar,
+  NickName,
+  Login,
+  Bio,
+  Others
+} from './styles'
+
+export const UserInfo: React.FC = () => {
+  const { user } = useContext(SearchContext)
+
+  return (
+    <Container>
+      {user ? (
+      <>
+        <Avatar
+          src={user?.avatar}
+          alt={`Avatar de ${user?.nickname}`}
+          draggable={false}
+        />
+        <NickName>{user.nickname}</NickName>
+        <Login href={`https://github.com/${user.login}`}>
+          <FaGithubAlt size="16" />
+          {user.login}
+        </Login>
+        <Bio>{user.bio}</Bio>
+        <Others>
+          {user.location ?
+            <div>
+              <FaMapMarkerAlt />
+              <p>{user.location}</p>
+            </div> : ''
+          }
+          {user.email ?
+            <div>
+              <FaRegEnvelope />
+              <p>{user.email}</p>
+            </div> : ''
+          }
+          {user.company ?
+            <div>
+              <FaRegBuilding />
+              <p>{user.company}</p>
+            </div> : ''
+          }
+          {user.blog ?
+            <div>
+              <FaLink />
+              <a
+                href={user.blog}
+                target="_blank"
+              >
+                {user.blog}
+              </a>
+            </div> : ''
+          }
+          {user.twitter ?
+            <div>
+              <FaTwitter />
+              <a
+                href={`https://twitter.com/${user.twitter}`}
+              >
+                @{user.twitter}
+              </a>
+            </div> : ''
+          }
+        </Others>
+      </>
+      ) : ''}
+    </Container>
+  )
+}
---
+++ src/components/UserInfo/styles.ts
@@ -0,0 +1,89 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 100vh;
+
+  margin: 0 20px;
+`
+
+export const Avatar = styled.img`
+  width: 50%;
+  max-width: 250px;
+  height: auto;
+
+  margin-bottom: 20px;
+
+  border: 2px solid var(--borderColor);
+  border-radius: 50%;
+
+  @media only screen and (max-width: 800px) {
+    max-width: 200px;
+  }
+`
+
+export const NickName = styled.h1`
+  margin: 0;
+
+  font-size: 30px;
+  color: var(--white);
+
+  @media only screen and (max-width: 650px) {
+    font-size: 27px;
+  }
+`
+
+export const Login = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+
+  margin: 0;
+
+  margin-bottom: 10px;
+
+  font-weight: normal;
+  color: var(--gray);
+  filter: opacity(0.7);
+`
+
+export const Bio = styled.p`
+  margin: 0;
+  margin-bottom: 10px;
+
+  max-width: 600px;
+
+  text-align: center;
+  color: var(--gray);
+`
+
+export const Others = styled.div`
+  display: flex;
+  color: var(--gray);
+  filter: opacity(0.7);
+  gap: 8px;
+
+  a {
+    color: var(--gray);
+    text-decoration: none;
+  }
+
+  @media only screen and (max-width: 800px) {
+    flex-direction: column;
+    justify-content: left;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-right: 3px;
+    }
+  }
+`
