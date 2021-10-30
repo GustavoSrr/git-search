9e95295a9a759a813abb62377130fb6656a4c6bc
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-30 17:32:29 -0300

    ✨ Usando context

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchProvider from "./contexts/search";
+
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchProvider>
+      <App />
+    </SearchProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
