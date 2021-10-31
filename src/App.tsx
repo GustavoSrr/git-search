736071e775e2d33459196aa37994a4763d22efd1
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-30 21:02:51 -0300

    ✨ Usando UserInfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { GlobalStyle } from "./styles/global"
 
-import Header from "./components/Header"
+import { Header } from "./components/Header"
+import { UserInfo } from "./components/UserInfo"
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Header />
+      <UserInfo />
     </div>
   )
 }
