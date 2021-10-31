8fa6df670f4dbf3f0d9d53cb79cf822ef753401a
Gustavo <gustavosrodrigues2000@gmail.com>
2021-10-31 17:29:42 -0300

    ✨ Mostrando header somente quando há um usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useContext } from "react"
+import { SearchContext } from "./contexts/search"
+
 import { GlobalStyle } from "./styles/global"
 
 import { Header } from "./components/Header"
+import { SearchBox } from "./components/SearchBox"
 import { UserInfo } from "./components/UserInfo"
 
 function App() {
+  const { user } = useContext(SearchContext)
+
   return (
     <div className="App">
       <GlobalStyle />
-      <Header />
-      <UserInfo />
+      {user ? <><Header /> <UserInfo /></> : <SearchBox />}
     </div>
   )
 }
